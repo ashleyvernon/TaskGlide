f105e39cf1109737a26be855c5fcdace79b550a9
ashleyvernon <avernon526@gmail.com>
2017-02-12 02:25:25 -0800

    changes color of logo to white

--- vendor/jquery/jquery.js
+++ vendor/jquery/jquery.js
@@ -11,6 +11,15 @@
  *
  * Date: 2016-05-20T17:17Z
  */
+$(window).scroll(function() {    
+   var scroll = $(window).scrollTop();
+
+    //>=, not <=
+   if (scroll >= 500) {
+       //clearHeader, not clearheader - caps H
+       $(".navbar").addClass(".navbar2");
+   }
+}); //missing );
 
 (function( global, factory ) {
 
